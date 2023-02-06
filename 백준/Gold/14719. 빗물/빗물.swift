b81eb39d3c53f62cf76d05f955ecbd42cb1bc27e
SOOM <gogotnals@naver.com>
2023-02-06 12:37:39 +0900

    [Gold V] Title: 빗물, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.swift
+++ 백준/Gold/14719. 빗물/빗물.swift
@@ -1,41 +1,14 @@
 import Foundation
 let n = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (H,W) = (n[0], n[1])
-var array: [[Int]] = Array(repeating: Array(repeating: 0, count: W), count: H)
-let m = readLine()!.split(separator: " ").map{Int(String($0))!}
-for i in 0...W-1{
-    for j in stride(from: H-1, to: H-1-m[i], by: -1){
-        array[j][i] = 1
+let arr: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+var result = 0
+for j in 1...W-2{
+    let left = (arr[..<j]).max()!
+    let right = (arr[j...]).max()!
+    let m = min(left, right)
+    if arr[j] < m {
+        result += m - arr[j]
     }
 }
-var bool = false
-var count: Int = 0
-var temp: Int = 0
-
-for i in 0...H-1{
-    for j in 0...W-2 {
-        if array[i][j] == 1{
-            if bool{
-                count += temp
-                temp = 0
-                
-            }
-            if array[i][j+1] == 0{
-                bool = true
-            }else{
-                bool = false
-            }
-        }
-        else{
-            if bool{
-                temp += 1
-            }
-        }
-    }
-    if array[i][array[i].endIndex-1] == 1{
-        count += temp
-    }
-    temp = 0
-    bool = false
-}
-print(count)
+print(result)
