8fd31a5acd1ed6ae064a49546e950b7110a536ca
SOOM <gogotnals@naver.com>
2023-04-12 17:55:14 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func solution(_ sequence:[Int], _ k:Int) -> [Int] {
+    var min = sequence.count+1
+    var (start, end) = (0, 0)
+    var result: [Int] = []
+    var sum = sequence[0]
+    while start < sequence.count {
+        if sum < k {
+            if end == sequence.count - 1 {
+                break
+            }
+            end += 1
+            sum += sequence[end]
+        }
+        else if sum > k {
+            sum -= sequence[start]
+            if start < end {
+                start += 1
+            }
+        }
+        else {
+            if end - start + 1 < min {
+                min = end - start + 1
+                result = [start, end]
+            }
+             if end == sequence.count - 1 {
+                break
+            }
+            end += 1
+            sum += sequence[end]
+        }
+    }
+   return result
+}
