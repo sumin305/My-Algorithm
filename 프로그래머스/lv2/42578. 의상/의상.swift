6f3b258ad0d1af45ebf3a612fb3415da0b05bf25
SOOM <gogotnals@naver.com>
2023-05-06 16:42:52 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    var category: [String:Int] = [:]
+    var result = 1
+    // 의상의 종류마다 의상의 개수 count
+    for cloth in clothes {
+        if let num = category[cloth[1]] {
+            category[cloth[1], default:0] += 1
+        } else {
+            category[cloth[1], default:0] = 1
+        }
+    }
+    print(category)
+    // 한 개의 이상으로 조합한 개수 세서 출력 
+    for cate in category {
+        result = result * (cate.value+1)
+        print(cate.value+1)
+    }
+    return result-1
+}
