97be9640781a781f096e05cee8430c47c75224a3
SOOM <gogotnals@naver.com>
2023-05-18 21:45:47 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    let citations = citations.sorted() { $0 > $1 }
+    var result = 0
+
+    for i in 0..<citations.count {
+        if i + 1 <= citations[i] {
+            result = i + 1
+        } else {
+            break
+        }
+    }
+    return result
+}
