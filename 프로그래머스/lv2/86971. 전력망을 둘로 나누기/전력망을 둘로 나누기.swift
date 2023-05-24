2e9f250f9c580cc6bdf4666717bb4710dbbd71b8
SOOM <gogotnals@naver.com>
2023-05-24 15:37:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.78 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+func solution(_ n:Int, _ wire:[[Int]]) -> Int {
+    var graph: [[Int]] = Array(repeating: [], count: n+1)
+    var visited: [Bool] = Array(repeating: false, count: n+1)
+    var resultArr: [Int] = []
+    
+    func dfs (_ first: Int, _ wire: [Int]) -> Int {
+        var queue: [Int] = [first]
+        var count = 0
+        while !queue.isEmpty {
+            let target = queue.removeFirst()
+            visited[target] = true
+            count += 1
+            
+            for adj in graph[target] {
+               if wire.contains(target) {
+                    if adj == wire[1 - wire.firstIndex(of: target)!] {
+                        continue
+                    }
+                }
+                if !visited[adj] {
+                   queue.append(adj)
+                }
+            }
+        }
+        return count
+    }
+    
+    for w in wire {
+        graph[w[0]].append(w[1])
+        graph[w[1]].append(w[0])
+    }
+    
+    for w in wire {
+        visited = Array(repeating: false, count: n+1)
+        var countArr: [Int] = []
+        for i in 1...n {
+            if !visited[i] {
+                countArr.append(dfs(i, w))
+            }
+        }
+        resultArr.append(abs(countArr[0]-countArr[1]))
+    }
+    
+    print(graph)
+    return resultArr.min()!
+}
