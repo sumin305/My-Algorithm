32e02c5fd1b791d2efa1e8d4a2c2e6c7ca741f29
SOOM <gogotnals@naver.com>
2023-05-25 15:07:16 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.swift
@@ -0,0 +1,25 @@
+func solution(_ tickets:[[String]]) -> [String] {
+    var result: [[String]] = []
+    func DFS(_ target: String, _ arr: [String], _ tickets: [[String]]) {
+
+        if tickets.isEmpty {
+            result.append(arr)
+        }
+        guard tickets.map({$0[0]}).contains(target) else { return }
+        for idx in 0..<tickets.count {
+            if tickets[idx][0] == target {
+                var tempArr = arr
+                tempArr.append(tickets[idx][1])
+                var tempTickets = tickets
+                tempTickets.remove(at: idx)
+                DFS(tickets[idx][1], tempArr, tempTickets)
+            }
+        }
+    }
+    DFS("ICN", ["ICN"], tickets)
+    if result.isEmpty {
+        return []
+    } else {
+        return result.sorted{String($0.flatMap{$0}) < String($1.flatMap{$0})}[0]
+    }
+}
