85ad5bc6ab1ff764b17204e82fd0bc8f1d5a2071
SOOM <gogotnals@naver.com>
2023-05-27 21:51:47 +0900

    [Silver I] Title: 스타트링크, Time: 24 ms, Memory: 80492 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.swift
+++ 백준/Silver/5014. 스타트링크/스타트링크.swift
@@ -1,39 +1,5 @@
 import Foundation
 
-struct Queue<T> {
-    var inbox: [T]
-    var outbox: [T] = []
-    var count: Int {
-        return inbox.count + outbox.count
-    }
-    var isEmpty: Bool {
-        return inbox.isEmpty && outbox.isEmpty
-    }
-    init(_ queue: [T]) {
-        inbox = queue
-    }
-    mutating func pushLast(_ element: T) {
-        inbox.append(element)
-    }
-    mutating func pushFirst(_ element: T) {
-        outbox.append(element)
-    }
-    mutating func popLast() -> T {
-        if inbox.isEmpty {
-            inbox = outbox.reversed()
-            outbox.removeAll()
-        }
-        return inbox.popLast()!
-    }
-    mutating func popFirst() -> T {
-        if outbox.isEmpty {
-            outbox = inbox.reversed()
-            inbox.removeAll()
-        }
-        return outbox.popLast()!
-    }
-}
-
 let n = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (F,S,G,U,D) = (n[0], n[1], n[2], n[3], n[4])
 
@@ -46,19 +12,20 @@ func canGo(_ num: Int) -> Bool {
     }
 }
 func BFS(_ start: Int) -> Int {
-    var queue = Queue([(num: start, depth: 0)])
+    var queue: [(num: Int, depth: Int)] = [(start, 0)]
     var visited: [Bool] = Array(repeating: false, count: F+1)
-    visited[start] = true
     let next: [Int] = [U, (-1)*D]
+    visited[start] = true
     while !queue.isEmpty {
-        let target = queue.popFirst()
+        let target = queue.removeFirst()
+        
         if target.num == G {
             return target.depth
         }
         for n in next {
             let nextNum = target.num + n
             if canGo(nextNum) && !visited[nextNum] {
-                queue.pushLast((nextNum, target.depth+1))
+                queue.append((nextNum, target.depth+1))
                 visited[nextNum] = true
             }
         }
