48dff92f2162dfa391de987bfdc379fea8604ce5
SOOM <gogotnals@naver.com>
2023-05-29 20:41:14 +0900

    [Silver III] Title: N과 M (1), Time: 116 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -4,12 +4,12 @@ let n = readLine()!.split(separator: " ").map{Int(String($0))!}
 
 let (N, M) = (n[0], n[1])
 
-var result: [[Int]] = []
 
 combi([], Array(1...N))
+
 func combi( _ arr: [Int], _ nums: [Int]) {
     if arr.count == M {
-        result.append(arr)
+        print(arr.map{String($0)}.joined(separator: " "))
         return
     }
     for idx in 0..<nums.count {
@@ -19,7 +19,4 @@ func combi( _ arr: [Int], _ nums: [Int]) {
         tempNums.remove(at: idx)
         combi(tempArr, tempNums)
     }
-}
-for r in result {
-    print(r.map{String($0)}.joined(separator: " "))
 }
