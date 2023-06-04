cb8c92a9cec3a6b9da0ad879128031d61c128e0b
SOOM <gogotnals@naver.com>
2023-06-04 23:06:37 +0900

    [Bronze II] Title: 8진수 2진수, Time: 204 ms, Memory: 97068 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.swift
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.swift
@@ -5,11 +5,9 @@ var str : [String] = []
 
 func eightToBin(_ num: [Character]) -> String {
    
-    
     for idx in stride(from: num.count-1, to: -1, by: -1) {
         tenToBin(Int(String(num[idx]))!)
     }
-
     let result = str.reversed().joined()
     if result == "000" {
         return "0"
@@ -19,8 +17,7 @@ func eightToBin(_ num: [Character]) -> String {
 }
 func tenToBin(_ num: Int) {
     let bin = [0: "000", 1: "001", 2: "010", 3: "011", 4: "100", 5: "101", 6: "110", 7: "111"]
-    
-    str.append(bin[num, default: "000"])
+     str.append(bin[num]!)
 }
 
 print(eightToBin(n))
