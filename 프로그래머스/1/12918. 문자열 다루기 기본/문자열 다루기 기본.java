287713a39a36c2b5312d6456c1b68f31762ffd22
SOOM <gogotnals@naver.com>
2025-01-01 17:32:32 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() != 4 && s.length() != 6) return false;
+
+        for (char c: s.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
+        }
+        return answer;
+    }
+}
