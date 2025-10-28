eb2c8999ef7ce2f2f4f1ec5ada04f509633b9f4c
SOOM <gogotnals@naver.com>
2025-10-28 19:35:14 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,43 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-	static boolean[] visited;
-	static int[] numbers;
-	static int N;
-	static int M;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		visited = new boolean[N + 1];
-		numbers = new int[M];
-		 
-		// 1~N 자연수 중 중복 없이 M개 고른 수열 (수열은 오름차순) -> 조합 
-		makeNums(0, 1);
-	}
-	
-	public static void makeNums(int idx, int start) {
-		if (idx == M) {
-			for (int i: numbers) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for (int num = start; num <= N; num++) {
-			if (!visited[num]) {
-				visited[num] = true;
-				numbers[idx] = num;
-				makeNums(idx + 1, num + 1);
-				visited[num] = false;
-			}
-		}
-	}
+
+    public static boolean[] visited;
+    public static int N, M, numbers[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = arr[0];
+        M = arr[1];
+        visited = new boolean[N + 1];
+        numbers = new int[M];
+
+        combination(0, 1);
+    }
+
+    // N개중에 M개 뽑는 조합
+    public static void combination(int idx, int start) {
+
+        if (idx == M) {
+            for (int num: numbers) System.out.print(num + " ");
+            System.out.println();
+            return;
+        }
+        for (int num = start; num <= N; num++) {
+
+            if (!visited[num]) {
+                visited[num] = true;
+                numbers[idx] = num;
+                combination(idx + 1, num + 1);
+                visited[num] = false;
+            }
+        }
+    }
 }
