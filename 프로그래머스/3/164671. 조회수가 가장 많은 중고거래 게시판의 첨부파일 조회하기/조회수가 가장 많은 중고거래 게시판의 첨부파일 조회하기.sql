eaee0cdf798928bce815d3586e6f31aa4bbe06fb
SOOM <gogotnals@naver.com>
2025-10-31 21:49:33 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,10 +1,7 @@
 -- 코드를 입력하세요
-SELECT concat('/home/grep/src/', b.board_id, '/',f.file_id, f.file_name,f.FILE_EXT) as file_path 
-from USED_GOODS_BOARD b
-join USED_GOODS_FILE f
-on b.board_id = f.board_id
-where b.views = (
-select max(VIEWS)
-          from USED_GOODS_BOARD
+SELECT CONCAT('/home/grep/src/', B.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) AS 'FILE_PATH'
+FROM USED_GOODS_BOARD AS B JOIN USED_GOODS_FILE AS F ON B.BOARD_ID  = F.BOARD_ID 
+WHERE B.VIEWS = (
+SELECT max(VIEWS) from USED_GOODS_BOARD
 )
-order by f.file_id desc
+ORDER BY F.FILE_ID DESC
